b0f1facd7f5bf9a618e4bd53092316ed1a668ca7
Lamkadam Hamza <hamza.lamkadam@mrbricolage.ma>
2024-11-08 10:42:57 +0100

    Initial commit

---
+++ src/main/java/com/example/tp2jenkins/Tp2jenkinsApplication.java
@@ -0,0 +1,22 @@
+package com.example.tp2jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class Tp2jenkinsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Tp2jenkinsApplication.class, args);
+  }
+
+
+  @GetMapping("/test")
+  public String hello() {
+    return "Hello World";
+  }
+}
